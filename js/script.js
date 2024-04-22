5978517e7fd1e9092ea63920d010d492349bf6d5
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-04-22 17:37:56 -0300

    closes #5 - add viacep request with javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+"use strict"
+
+let cepData = ""
+
 const cepField = document.getElementById("cep");
+const cepFeedback = document.getElementById("cepFeedback");
 
 $('#cep').mask('00000-000');
 
 cepField.addEventListener("focus", (event) => {
   if ((event.target.value).length < 9) {
+    cepFeedback.innerHTML = "Insira um CEP válido!";
     cepField.classList.add("is-invalid");
   }
 })
@@ -13,10 +19,19 @@ cepField.addEventListener("input", (event) => {
     cepField.classList.remove("is-invalid")
   }
   if ((event.target.value).length < 9) {
+    cepFeedback.innerHTML = "Insira um CEP válido!";
     cepField.classList.add("is-invalid")
   }
 })
 
-cepField.addEventListener("blur", (event) => {
+cepField.addEventListener("blur", async (event) => {
   const cep = (event.target.value).replace("-", "");
+  if (cep.length === 9) {
+    const response = await getCEP(cep);
+  }
 })
+
+async function getCEP(cep) {
+  const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  return response.data;
+}
