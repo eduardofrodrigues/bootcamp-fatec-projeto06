98976b2d335e77ccab9e8cd83aef38045249a155
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-04-22 21:49:27 -0300

    closes #8 - add data into table

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 "use strict"
 
+$('#cep').mask('00000-000');
+
+const cadastrados = []
+
 let cepData = ""
 
 const cepField = document.getElementById("cep");
 const cepFeedback = document.getElementById("cepFeedback");
-const saveButton = document.getElementById("saveButton");
-
-$('#cep').mask('00000-000');
+const table = document.getElementById("tableBody");
+const form = document.getElementById("form");
 
 cepField.addEventListener("focus", (event) => {
   if ((event.target.value).length < 9) {
@@ -65,3 +68,42 @@ async function getCEP(cep) {
   const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
   return response.data;
 }
+
+function salvarCadastro() {
+  const name = document.getElementById("name");
+  const surname = document.getElementById("surname");
+  const cep = document.getElementById("cep");
+  const address = document.getElementById("address");
+  const number = document.getElementById("number");
+  const neighborhood = document.getElementById("neighborhood");
+  const city = document.getElementById("city");
+  const province = document.getElementById("province");
+
+  const cadastro = {
+    id: (cadastrados.length + 1),
+    name: `${name.value} ${surname.value}`,
+    address: `${address.value}, ${number.value}`,
+    cep: cep.value,
+    neighborhood: neighborhood.value,
+    city: city.value,
+    province: province.value
+  }
+  cadastrados.push(cadastro);
+  addDataIntoTable(cadastro);
+  form.reset();
+}
+
+function addDataIntoTable(cadastro) {
+  table.innerHTML += `
+  <tr>
+    <th scope="row">${cadastro.id}</th>
+    <td>${cadastro.name}</td>
+    <td>${cadastro.address}</td>
+    <td>${cadastro.cep}</td>
+    <td>${cadastro.neighborhood}</td>
+    <td>${cadastro.city}</td>
+    <td>${cadastro.province}</td>
+  </tr>
+  `
+}
+
